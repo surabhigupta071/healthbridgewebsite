b9da5187d0155dba88974b2ed484093cdac11bb0
surabhigupta071 <126202819+surabhigupta071@users.noreply.github.com>
2025-09-10 20:53:39 -0400

    Update genkit.ts

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -3,6 +3,7 @@ import {googleAI} from '@genkit-ai/googleai';
 
 export const ai = genkit({
   plugins: [googleAI()],
-  model: 'googleai/gemini-2.5-flash',
+  model: 'googleai/gemini-1.5-flash-latest',
 });
+
 //look over this page again
